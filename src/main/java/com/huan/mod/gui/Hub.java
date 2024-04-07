203f4718ad8a98a704d7ecd717b04eb2e95d45d6
Huan-zhaojun <111294954+Huan-zhaojun@users.noreply.github.com>
2024-04-07 19:39:27 +0800

    add and modify 修正了hub的相对位置

--- src/main/java/com/huan/mod/gui/Hub.java
+++ src/main/java/com/huan/mod/gui/Hub.java
@@ -11,12 +11,15 @@ public class Hub extends AbstractGui {
     private final int width;
     private final int height;
     private final Minecraft minecraft;
-    private final ResourceLocation HUD = new ResourceLocation(fart_shit_pee.MOD_ID, "textures/gui/a.png");
+    private final ResourceLocation bladder_Hub = new ResourceLocation(fart_shit_pee.MOD_ID, "textures/gui/bladder_hub.png");
+    private final ResourceLocation intestine_Hub = new ResourceLocation(fart_shit_pee.MOD_ID, "textures/gui/intestine_hub.png");
     private final MatrixStack matrixStack;
 
     public Hub(MatrixStack matrixStack) {
         this.width = Minecraft.getInstance().getMainWindow().getScaledWidth();
         this.height = Minecraft.getInstance().getMainWindow().getScaledHeight();
+        System.out.println(width);
+        System.out.println(height);
         this.minecraft = Minecraft.getInstance();
         this.matrixStack = matrixStack;
     }
@@ -25,9 +28,18 @@ public class Hub extends AbstractGui {
         //RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
-        this.minecraft.getTextureManager().bindTexture(HUD);
 
-        blit(matrixStack, width / 2, height / 2, 0, 0, 32, 32, 32, 32);
+        //绘制膀胱
+        int bladder_x = width / 2 - 125, bishop_y = height - 35;
+        this.minecraft.getTextureManager().bindTexture(bladder_Hub);
+        blit(matrixStack, bladder_x, bishop_y, 0, 0, 32, 32, 64, 32);
+        blit(matrixStack, bladder_x, bishop_y, 32, 0, 32, 32, 64, 32);
+
+        //绘制肠道
+        int intestine_x = width / 2 + 95, intestine_y = height - 35;
+        this.minecraft.getTextureManager().bindTexture(intestine_Hub);
+        blit(matrixStack, intestine_x, intestine_y, 0, 0, 32, 32, 352, 32);
+        blit(matrixStack, intestine_x, intestine_y, 32, 0, 32, 32, 352, 32);
 
         RenderSystem.disableBlend();
     }
